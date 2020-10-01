e221d4e71d5578d331df3d87cd47676f5d15a217
Arpit Sahu <56678108+Arpit-Sahu@users.noreply.github.com>
2020-10-01 11:35:21 +0530

    updated code

--- func_largest3.c
+++ func_largest3.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 int largest (int x, int y,int z);
-int x,y,z,o,n,m,large,p=-32767;
+int x,y,z;
 void main()
 {
- printf("Enter the the three numbers to compare");
+ printf("Enter the the three numbers to compare\n");
  scanf("%d%d%d",&x,&y,&z);
- large=largest(x,y,z);
- printf("Largest no. among the three is %d",large); 
+ printf("Largest no. among the three is %d",largest(x,y,z));
 }
 
 int largest (int m, int n,int o)
 {
-  if(m>p)
-  p=m;
-  else if(n>p)
-  p=n;
-  else if(o>p)
-  p=o;
+  int p = m;
+  if(p<n)
+    p = n;
+  if(p<o)
+    p = o;
   return p;
 }
